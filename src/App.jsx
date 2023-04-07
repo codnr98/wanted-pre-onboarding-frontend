ebfc58806021c2a6eec59d5680aec67f83d7d6d7
codnr <wnd75913@gmail.com>
2023-04-07 10:58:32 +0900

    chore: react router dom 설치

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
+import React from "react";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
 import Input from "./components/Input";
+import Main from "./pages/Main";
 
 function App() {
   return (
     <div>
       <h1 className='text-3xl'>ToDoList</h1>
       <Input placeholder={"Email"} label={"Email"} id={"email"}></Input>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Main />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
