da39a4e7857f22fec698026c76eb09a30f1c91e8
codnr <wnd75913@gmail.com>
2023-04-07 14:13:24 +0900

    feat: ToDoList page 생성 및 router 연결

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import {BrowserRouter, Route, Routes} from "react-router-dom";
 import Main from "./pages/Main";
 import SignUp from "./pages/SignUp";
 import SignIn from "./pages/SignIn";
+import ToDoList from "./pages/ToDoList";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path='/' element={<Main />} />
+          <Route path='/todo' element={<ToDoList />} />
           <Route path='/signup' element={<SignUp />} />
           <Route path='/signin' element={<SignIn />} />
         </Routes>
