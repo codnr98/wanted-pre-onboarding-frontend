1b567aa208b1171bc56f457fdd786f9645ac754e
codnr <wnd75913@gmail.com>
2023-04-07 14:46:26 +0900

    feat: ToDoList page 생성

--- src/pages/ToDoList.jsx
+++ src/pages/ToDoList.jsx
@@ -1,7 +1,33 @@
+import {useEffect, useState} from "react";
+
 function ToDoList() {
+  const [toDos, setToDos] = useState([]);
+
+  const patchToDoList = async () => {};
+
+  useEffect(() => {
+    setToDos();
+  }, []);
+
   return (
     <div>
       <h1 className='text-3xl'>ToDo List</h1>
+
+      <form>
+        <input data-testid='new-todo-input' />
+        <button data-testid='new-todo-add-button'>추가</button>
+      </form>
+
+      <ul>
+        <li>
+          <label>
+            <input type='checkbox' />
+            <span>TODO 1</span>
+          </label>
+          <button data-testid='modify-button'>수정</button>
+          <button data-testid='delete-button'>삭제</button>
+        </li>
+      </ul>
     </div>
   );
 }
