00ea61dd060c34d5e22c68f2cd08caf903f56aea
codnr <wnd75913@gmail.com>
2023-04-07 13:42:23 +0900

    feat: 회원가입 api 요청하는 함수 postSignUp 구현

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -3,18 +3,28 @@ import EmailInput from "../components/EmailInput";
 import validationEmail from "../utils/validationEmail";
 import PasswordInput from "../components/PasswordInput";
 import validationPassword from "../utils/validationPassword";
+import postSignUp from "../apis/postSignUp";
+import {useNavigate} from "react-router";
 
 function SignUp() {
   const [email, setEmail] = useState("");
   const [isEmailSuccess, setIsEmailSuccess] = useState({
-    isSuccess: true,
+    isSuccess: false,
     errorMessage: "",
   });
   const [password, setPassword] = useState("");
   const [isPasswordSuccess, setIsPasswordSuccess] = useState({
-    isSuccess: true,
+    isSuccess: false,
     errorMessage: "",
   });
+
+  const isSuccess = isEmailSuccess.isSuccess && isPasswordSuccess.isSuccess;
+  const navigate = useNavigate();
+
+  const pathData = {
+    email: "",
+    password: "",
+  };
 
   const handleChangeEmail = (e) => {
     const {value} = e.target;
@@ -28,21 +38,45 @@ function SignUp() {
     validationPassword(value, setIsPasswordSuccess);
   };
 
+  const handleClick = async (e) => {
+    e.preventDefault();
+    validationEmail(email, setIsEmailSuccess);
+    validationPassword(password, setIsPasswordSuccess);
+    pathData.email = email;
+    pathData.password = password;
+    try {
+      await postSignUp(pathData);
+      navigate("/signin");
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <div>
       <h1 className='text-3xl'>Sign Up</h1>
-      <EmailInput
-        id={"signup__email-input"}
-        value={email}
-        onChange={handleChangeEmail}
-        errorMessage={isEmailSuccess.errorMessage}
-      />
-      <PasswordInput
-        id={"signup__password-input"}
-        value={password}
-        onChange={handleChangePassword}
-        errorMessage={isPasswordSuccess.errorMessage}
-      />
+      <form>
+        <EmailInput
+          id={"signup__email-input"}
+          value={email}
+          onChange={handleChangeEmail}
+          errorMessage={isEmailSuccess.errorMessage}
+        />
+        <PasswordInput
+          id={"signup__password-input"}
+          value={password}
+          onChange={handleChangePassword}
+          errorMessage={isPasswordSuccess.errorMessage}
+        />
+        <button
+          className='border p-2 rounded-lg bg-blue-500 text-white hover:bg-blue-600 disabled:bg-blue-300'
+          onClick={handleClick}
+          type='submit'
+          data-testid='signup-button'
+          disabled={isSuccess ? null : "disabled"}>
+          Sign Up
+        </button>
+      </form>
     </div>
   );
 }
