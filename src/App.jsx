109c17ed637c715440ac2e0aa3a1190ee6967191
codnr <wnd75913@gmail.com>
2023-04-14 14:16:07 +0900

    fix: hash router v6버전에 맞게 수정

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import {BrowserRouter, HashRouter, Route, Switch} from "react-router-dom";
+import {Route, Routes} from "react-router-dom";
 import Main from "./pages/Main";
 import SignUp from "./pages/SignUp";
 import SignIn from "./pages/SignIn";
@@ -7,14 +7,12 @@ import ToDoList from "./pages/ToDoList";
 function App() {
   return (
     <div className='flex p-4 w-96'>
-      <HashRouter basename='wanted-pre-onboarding-frontend'>
-        <Switch>
-          <Route path='/' element={<Main />} />
-          <Route path='/todo' element={<ToDoList />} />
-          <Route path='/signup' element={<SignUp />} />
-          <Route path='/signin' element={<SignIn />} />
-        </Switch>
-      </HashRouter>
+      <Routes>
+        <Route path='/#' element={<Main />} />
+        <Route path='/#/todo' element={<ToDoList />} />
+        <Route path='/#/signup' element={<SignUp />} />
+        <Route path='/#/signin' element={<SignIn />} />
+      </Routes>
     </div>
   );
 }
