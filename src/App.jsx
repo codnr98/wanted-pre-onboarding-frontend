264af0d5265613aa401bb2a9029096af65059192
codnr <wnd75913@gmail.com>
2023-04-14 17:01:54 +0900

    chore: hompage 값 수정

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import {Route, Routes} from "react-router-dom";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
 import Main from "./pages/Main";
 import SignUp from "./pages/SignUp";
 import SignIn from "./pages/SignIn";
@@ -7,12 +7,14 @@ import ToDoList from "./pages/ToDoList";
 function App() {
   return (
     <div className='flex p-4 w-96'>
-      <Routes>
-        <Route path='/#' element={<Main />} />
-        <Route path='/#/todo' element={<ToDoList />} />
-        <Route path='/#/signup' element={<SignUp />} />
-        <Route path='/#/signin' element={<SignIn />} />
-      </Routes>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Main />} />
+          <Route path='/todo' element={<ToDoList />} />
+          <Route path='/signup' element={<SignUp />} />
+          <Route path='/signin' element={<SignIn />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
