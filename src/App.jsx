6003b22952843955286ff471f9fe53a30e7270d9
codnr <wnd75913@gmail.com>
2023-04-14 14:12:14 +0900

    chore: BroswerRouter에서 HashRouter로 변경

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import {BrowserRouter, Route, Routes} from "react-router-dom";
+import {BrowserRouter, HashRouter, Route, Switch} from "react-router-dom";
 import Main from "./pages/Main";
 import SignUp from "./pages/SignUp";
 import SignIn from "./pages/SignIn";
@@ -7,14 +7,14 @@ import ToDoList from "./pages/ToDoList";
 function App() {
   return (
     <div className='flex p-4 w-96'>
-      <BrowserRouter basename='wanted-pre-onboarding-frontend'>
-        <Routes>
+      <HashRouter basename='wanted-pre-onboarding-frontend'>
+        <Switch>
           <Route path='/' element={<Main />} />
           <Route path='/todo' element={<ToDoList />} />
           <Route path='/signup' element={<SignUp />} />
           <Route path='/signin' element={<SignIn />} />
-        </Routes>
-      </BrowserRouter>
+        </Switch>
+      </HashRouter>
     </div>
   );
 }
